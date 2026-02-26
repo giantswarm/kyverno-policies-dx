bcffd98c4f1f3d11699e39b97d92a43e71753519
Franco <franco@giantswarm.io>
2026-02-26 12:28:17 -0300

    Fix workflow

---
+++ Makefile.custom.mk
@@ -0,0 +1,22 @@
+SHELL:=/usr/bin/env bash
+
+##@ Generate
+
+.PHONY: generate
+generate: ## Replace variables on Helm manifests.
+	./hack/template.sh
+
+.PHONY: verify
+verify:
+	@$(MAKE) generate
+	git diff --exit-code
+
+##@ Test
+
+.PHONY: clean
+clean: ## Delete test manifests from kind cluster.
+	./hack/cleanup-local.sh
+
+.PHONY: dabs
+dabs: generate
+	dabs.sh --generate-metadata --chart-dir helm/kyverno-policies-dx
